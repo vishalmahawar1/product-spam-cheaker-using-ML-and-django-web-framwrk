2b0ea7aff2db6d0c235f560dbd385deb490edf9e
vishalmahawar1 <66195603+vishalmahawar1@users.noreply.github.com>
2021-02-05 12:07:07 +0530

    Update settings.py

--- spam/settings.py
+++ spam/settings.py
@@ -129,8 +129,8 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'qwrasdzxczxcasdqwe@gmail.com'
-EMAIL_HOST_PASSWORD = 'Vishal@123'
+EMAIL_HOST_USER = 'your-gmail-ig'
+EMAIL_HOST_PASSWORD = 'your-gmail-password'
 
 
 
